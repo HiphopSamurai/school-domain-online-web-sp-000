56fcc8263375e4aa1de29115058ef276c2f66395
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-02 02:14:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+
+  #attr_reader :name
+
+  #@ROSTER = {}
+  def initialize(name)
+    @name = name
+    @ROSTER = {}
+  end
+
+  def name
+    @name
+  end
+
+  def roster=(roster)
+    @ROSTER << roster
+  end
+
+  def roster
+    @ROSTER
+  end
+
+  def add_student(name, grade)
+    @ROSTER[grade] = name
+  end
+
+  def grade(grade)
+    @ROSTER.values_at(grade)
+  end
+
+  def sort
+    @ROSTER.sort
+  end
+end
